fb6ef3743979bba76f450b4e12de578fd67c4d01
Aaron Spivak <69207934+aspivak5@users.noreply.github.com>
2021-02-02 19:35:40 -0500

    setup

--- NBA/app.py
+++ NBA/app.py
@@ -1,1 +1,29 @@
+# import necessary libraries
 import os
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+# Database Setup
+#################################################
+
+
+# create route that renders index.html template
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
