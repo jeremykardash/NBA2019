f36dc5f9bd041675c4d41eb07e2ec246c8b6c7dc
Aaron Spivak <69207934+aspivak5@users.noreply.github.com>
2021-02-05 13:09:58 -0500

    created dropmenu

--- NBA/static/js/stats.js
+++ NBA/static/js/stats.js
@@ -4,5 +4,28 @@ d3.json(url).then(function(response){
     console.log(response)
 })
 }
-
 test();
+
+function dropdown(){
+    var url ="/api/stats"
+    var dropdownone = d3.select("#selstatone")
+    var dropdowntwo = d3.select("#selstattwo")
+
+    d3.json(url).then(function(response){
+        console.log(Object.keys(response[0]))
+        var keys = Object.keys(response[0])
+
+        keys.forEach((key)=>{
+            dropdownone.append("option").text(key).property("value",key)
+        });
+        keys.forEach((key)=>{
+            dropdowntwo.append("option").text(key).property("value",key)
+        });
+    })
+}
+dropdown()
+
+function buildChart(){
+    
+
+}
