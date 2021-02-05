3630bf0a2e8c82a3d98684a739a27a4d488f58d8
jeremykardash <jer.kardash@gmail.com>
2021-02-04 20:40:34 -0500

    heroku push

--- NBA/app.py
+++ NBA/app.py
@@ -4,6 +4,8 @@ import os
 from flask.globals import session
 from flask_sqlalchemy import SQLAlchemy
 
+#from models import create_classes
+
 from flask import (
     Flask,
     render_template,
@@ -25,30 +27,33 @@ app = Flask(__name__)
 
 # app.config['SQLALCHEMY_DATABASE_URL'] = os.environ.get('DATABASE_URL', '')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/nba2018db"
-db = SQLAlchemy(app)
+#app.config['SQLALCHEMY_DATABASE_URL'] = "postgres://gvuvmkxy:Z62u_yZyL3sTjlr-XDy0eUcBrAy9ucOU@ziggy.db.elephantsql.com:5432/gvuvmkxy"
+#db = SQLAlchemy(app)
 
-engine = create_engine("postgresql://postgres:postgres@localhost:5432/nba2018db")
+engine = create_engine("postgres://gvuvmkxy:Z62u_yZyL3sTjlr-XDy0eUcBrAy9ucOU@ziggy.db.elephantsql.com:5432/gvuvmkxy")
+
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
-print(Base.classes.keys())
+#Tables for queries
+combine = Base.classes.combine
+players = Base.classes.players
+stats = Base.classes.stats
+teams = Base.classes.teams
+salary = Base.classes.salaries
+players_team = Base.classes.players_teams
+
 
 # create route that renders index.html template
 @app.route("/")
 def home():
     return render_template("index.html")
-    
-combine = Base.classes.combine
-
-stats = Base.classes.stats
-
-Salary = Base.classes.salary
 
 
 # Service Routes
 @app.route("/api/main")
 def firstRoute():
+    session = Session(engine)
     data = session.query(combine.player_id, stats.player_name, stats.pos).filter(combine.player_id == stats.player_id).all()
     return jsonify(data)
 
@@ -56,8 +61,8 @@ def firstRoute():
 @app.route("/api/stats")
 def bubbleroute():
     session = Session(engine)
-    sel = [stats.player_id, stats.player_name,Salary.salary, stats.pos,stats.TwoP_m, stats.ThreeP_m]
-    results = session.query(*sel).filter(Salary.player_id == stats.player_id).all()
+    sel = [stats.player_id, stats.player_name,salary.salary, stats.pos,stats.TwoP_m, stats.ThreeP_m]
+    results = session.query(*sel).filter(salary.player_id == stats.player_id).all()
     session.close()
     all_stats = []
     for player_id, player_name, salary, pos, TwoP_m, ThreeP_m in results:
@@ -69,26 +74,6 @@ def bubbleroute():
         stats_dict["2pm"] = TwoP_m
         stats_dict["3pm"] = ThreeP_m
         all_stats.append(stats_dict)
-
-    # player_id = [result[0] for result in results]
-    # player_name = [result[1] for result in results]
-    # salary = [result[2] for result in results]
-    # position = [result[3] for result in results]
-    # TwoP_m= [result[4] for result in results]
-    # ThreeP_m = [result[5] for result in results]
-
-    # pet_data = [{
-    #     "player_info":{
-    #         "player_name":player_name,
-    #         "player_id": player_id,
-    #         "salary":salary,
-    #         "position":position,
-    #         "stats":{
-    #             "2pm":TwoP_m,
-    #             "3pm":ThreeP_m
-    #         }
-    #     }
-    # }]
     
     return jsonify(all_stats)
 
