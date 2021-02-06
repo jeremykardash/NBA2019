620c8bfcd6928ff915267f5b78c2876986e26de0
Matt Davies <matt.davies1@mac.com>
2021-02-06 13:06:08 -0500

    Reconfig chord.js

--- NBA/static/js/chord.js
+++ NBA/static/js/chord.js
@@ -50,141 +50,141 @@ var chord_options = {
 };
 
 // define function that draws chord diagram
-    function Chord(options, matrix) {
+function Chord(options, matrix) {
 
-        // initialize the chord configuration variables
-        var config = {
-            width: 640,
-            height: 560,
-            rotation: 0,
-            textgap: 20,
-            colors: colours1
-        };
-        
-        // add options to the chord configuration object
-        if (options) {
-            extend(config, options);
-        }
-        
-        // set chord visualization variables from the configuration object
-        var offset = Math.PI * config.rotation,
-            width = config.width,
-            height = config.height,
-            textgap = config.textgap,
-            colors = config.colors;
-        
-        // set viewBox and aspect ratio to enable a resize of the visual dimensions 
-        var viewBoxDimensions = "0 0 " + width + " " + height,
-            aspect = width / height;
-        
-        if (config.gnames) {
-            gnames = config.gnames;
-        } else {
-            // make a list of names
-            gnames = [];
-            for (var i=97; i<matrix.length; i++) {
-                gnames.push(String.fromCharCode(i));
-            }
-        }
-
-        // start the d3 magic
-        var chord = d3.layout.chord()
-            .padding(.05)
-            .sortSubgroups(d3.descending)
-            .matrix(matrix);
-
-        var innerRadius = Math.min(width, height) * .31,
-            outerRadius = innerRadius * 1.05;
-
-        var fill = d3.scale.ordinal()
-            .domain(d3.range(matrix.length-1))
-            .range(colors);
+    // initialize the chord configuration variables
+    var config = {
+        width: 640,
+        height: 560,
+        rotation: 0,
+        textgap: 20,
+        colors: colours1
+    };
     
-        var svg = d3.select("#chord-chart").append("svg")
-            .attr("id", "visual")
-            .attr("viewBox", viewBoxDimensions)
-            .attr("preserveAspectRatio", "xMinYMid")    // add viewBox and preserveAspectRatio
-            .attr("width", width)
-            .attr("height", height)
-          .append("g")
-            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-        var g = svg.selectAll("g.group")
-            .data(chord.groups)
-          .enter().append("svg:g")
-            .attr("class", "group");
-
-        g.append("svg:path")
-            .style("fill", function(d) { return fill(d.index); })
-            .style("stroke", "black")
-            .attr("id", function(d, i) { return "group" + d.index; })
-            .attr("d", d3.arc().innerRadius(innerRadius).outerRadius(outerRadius).startAngle(startAngle).endAngle(endAngle))
-            .on("mouseover", fade(.1))
-            .on("mouseout", fade(1));
-
-        g.append("svg:text")
-            .each(function(d) {d.angle = ((d.startAngle + d.endAngle) / 2) + offset; })
-            .attr("dy", ".35em")
-            .attr("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
-            .attr("transform", function(d) {
-                return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
-                    + "translate(" + (outerRadius + textgap) + ")"
-                    + (d.angle > Math.PI ? "rotate(180)" : "");
-              })
-            .text(function(d) { return gnames[d.index]; });
-
-        svg.append("g")
-            .attr("class", "chord")
-          .selectAll("path")
-            .data(chord.chords)
-          .enter().append("path")
-            .attr("d", d3.ribbon()
-            .radius(innerRadius)
-          )
-            .style("fill", function(d) { return fill(d.source.index); })
-            .style("opacity", 1)
-          .append("svg:title")
-            .text(function(d) { 
-                return  d.source.value + "  " + gnames[d.source.index] + " shared with " + gnames[d.target.index]; 
-            });
-    
-        // helper functions start here
-        
-        function startAngle(d) {
-            return d.startAngle + offset;
-        }
-
-        function endAngle(d) {
-            return d.endAngle + offset;
-        }
-        
-        function extend(a, b) {
-            for( var i in b ) {
-                a[ i ] = b[ i ];
-            }
-        }
-
-        // Returns an event handler for fading a given chord group.
-        function fade(opacity) {
-            return function(g, i) {
-                svg.selectAll(".chord path")
-                    .filter(function(d) { return d.source.index != i && d.target.index != i; })
-                    .transition()
-                    .style("opacity", opacity);
-            };
-        }
-        
-        
-        window.onresize = function() {
-            var targetWidth = (window.innerWidth < width)? window.innerWidth : width;
-            
-            var svg = d3.select("#visual")
-                .attr("width", targetWidth)
-                .attr("height", targetWidth / aspect);
-        }
-
-        
+    // add options to the chord configuration object
+    if (options) {
+        extend(config, options);
     }
+    
+    // set chord visualization variables from the configuration object
+    var offset = Math.PI * config.rotation,
+        width = config.width,
+        height = config.height,
+        textgap = config.textgap,
+        colors = config.colors;
+    
+    // set viewBox and aspect ratio to enable a resize of the visual dimensions 
+    var viewBoxDimensions = "0 0 " + width + " " + height,
+        aspect = width / height;
+    
+    if (config.gnames) {
+        gnames = config.gnames;
+    } else {
+        // make a list of names
+        gnames = [];
+        for (var i=97; i<matrix.length; i++) {
+            gnames.push(String.fromCharCode(i));
+        }
+    }
+
+    // D3 chord drawing
+    var chord = d3.layout.chord()
+        .padding(.05)
+        .sortSubgroups(d3.descending)
+        .matrix(matrix);
+
+    var innerRadius = Math.min(width, height) * .31,
+        outerRadius = innerRadius * 1.05;
+
+    var fill = d3.scale.ordinal()
+        .domain(d3.range(matrix.length-1))
+        .range(colors);
+
+    var svg = d3.select("#chord-chart").append("svg")
+        .attr("id", "visual")
+        .attr("viewBox", viewBoxDimensions)
+        .attr("preserveAspectRatio", "xMinYMid")    // add viewBox and preserveAspectRatio
+        .attr("width", width)
+        .attr("height", height)
+      .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    var g = svg.selectAll("g.group")
+        .data(chord.groups)
+      .enter().append("svg:g")
+        .attr("class", "group");
+
+    g.append("svg:path")
+        .style("fill", function(d) { return fill(d.index); })
+        .style("stroke", "black")
+        .attr("id", function(d, i) { return "group" + d.index; })
+        .attr("d", d3.arc().innerRadius(innerRadius).outerRadius(outerRadius).startAngle(startAngle).endAngle(endAngle))
+        .on("mouseover", fade(.1))
+        .on("mouseout", fade(1));
+
+    g.append("svg:text")
+        .each(function(d) {d.angle = ((d.startAngle + d.endAngle) / 2) + offset; })
+        .attr("dy", ".35em")
+        .attr("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
+        .attr("transform", function(d) {
+            return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
+                + "translate(" + (outerRadius + textgap) + ")"
+                + (d.angle > Math.PI ? "rotate(180)" : "");
+          })
+        .text(function(d) { return gnames[d.index]; });
+
+    svg.append("g")
+        .attr("class", "chord")
+      .selectAll("path")
+        .data(chord.chords)
+      .enter().append("path")
+        .attr("d", d3.ribbon()
+        .radius(innerRadius)
+      )
+        .style("fill", function(d) { return fill(d.source.index); })
+        .style("opacity", 1)
+      .append("svg:title")
+        .text(function(d) { 
+            return  d.source.value + "  " + gnames[d.source.index] + " shared with " + gnames[d.target.index]; 
+        });
+
+    // helper functions start here
+    
+    function startAngle(d) {
+        return d.startAngle + offset;
+    }
+
+    function endAngle(d) {
+        return d.endAngle + offset;
+    }
+    
+    function extend(a, b) {
+        for( var i in b ) {
+            a[ i ] = b[ i ];
+        }
+    }
+
+    // Returns an event handler for fading a given chord group.
+    function fade(opacity) {
+        return function(g, i) {
+            svg.selectAll(".chord path")
+                .filter(function(d) { return d.source.index != i && d.target.index != i; })
+                .transition()
+                .style("opacity", opacity);
+        };
+    }
+    
+    
+    window.onresize = function() {
+        var targetWidth = (window.innerWidth < width)? window.innerWidth : width;
+        
+        var svg = d3.select("#visual")
+            .attr("width", targetWidth)
+            .attr("height", targetWidth / aspect);
+    }
+
+    
+}
 
 window.onload = function() {
     Chord(chord_options, matrix);
