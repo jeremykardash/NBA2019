e057a50110bf9e19ed6b3ea0f749023bc9a9ad7a
jasperthegeo <70967011+jasperthegeo@users.noreply.github.com>
2021-02-06 13:03:51 -0500

    debug

--- NBA/static/js/html_design.js
+++ NBA/static/js/html_design.js
@@ -1,8 +1,26 @@
+window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      document.getElementById("header").style.fontSize = "30px";
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 15) {
+      document.getElementById("header_scroll").style.fontSize = "30px";
+      document.getElementById("header_scroll").style.height = "100px";
     } else {
-      document.getElementById("header").style.fontSize = "90px";
+      document.getElementById("header_scroll").style.fontSize = "90px";
+      document.getElementById("header_scroll").style.height = "300px";
     }
+    console.log("Hello");
   }
+
+
+
+//   $(document).on("scroll", function(){
+//       console.log("hello")
+//     if($(document).scrollTop() > 100)
+//     {
+//        $("#logo").addClass("shrink");
+//     }
+//     else
+//     {
+//         $("#logo").removeClass("shrink");
+//     }
+// });
