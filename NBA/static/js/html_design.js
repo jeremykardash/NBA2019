59b8aa9e0f4609dca54c55cca058291f9313371b
jasperthegeo <70967011+jasperthegeo@users.noreply.github.com>
2021-02-06 10:30:53 -0500

    shrinking banner

---
+++ NBA/static/js/html_design.js
@@ -0,0 +1,8 @@
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.getElementById("header").style.fontSize = "30px";
+    } else {
+      document.getElementById("header").style.fontSize = "90px";
+    }
+  }
