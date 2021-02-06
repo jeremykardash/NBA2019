9da62eb2515aa79428d0ea588d012483c7d3a0a3
jasperthegeo <70967011+jasperthegeo@users.noreply.github.com>
2021-02-06 13:20:56 -0500

    whatevs

--- NBA/static/js/html_design.js
+++ NBA/static/js/html_design.js
@@ -3,24 +3,10 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 15) {
       document.getElementById("header_scroll").style.fontSize = "30px";
-      document.getElementById("header_scroll").style.height = "100px";
+      document.getElementById("header_scroll").style.height = "40px";
     } else {
       document.getElementById("header_scroll").style.fontSize = "90px";
-      document.getElementById("header_scroll").style.height = "300px";
+      document.getElementById("header_scroll").style.height = "240px";
     }
     console.log("Hello");
   }
-
-
-
-//   $(document).on("scroll", function(){
-//       console.log("hello")
-//     if($(document).scrollTop() > 100)
-//     {
-//        $("#logo").addClass("shrink");
-//     }
-//     else
-//     {
-//         $("#logo").removeClass("shrink");
-//     }
-// });
